c83694828dc0198c98b361d27ee4197fad94a032
Shivam Mistry <shivam.mistry@12taste.com>
2026-07-26 00:33:05 +0530

    fix: set default values for subscription_id, tenant_id, and deployment variables

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,29 +1,35 @@
 variable "subscription_id" {
   type        = string
-  description = "The Azure Subscription ID"
+  description = "Azure Subscription ID"
+  default     = "0097149f-53b8-4433-869e-feb6129dbdc5"
 }
 
 variable "tenant_id" {
   type        = string
-  description = "The Azure Tenant ID or Domain"
+  description = "Azure Tenant ID or Domain Name"
+  default     = "jasperschouten12taste.onmicrosoft.com"
 }
 
 variable "location" {
   type        = string
+  description = "Azure Region"
   default     = "centralindia"
 }
 
 variable "resource_group_name" {
   type        = string
+  description = "Resource Group Name"
   default     = "rg-12taste-lakehouse-prod"
 }
 
 variable "storage_prefix" {
   type        = string
+  description = "Prefix for Storage Accounts"
   default     = "smistry12t"
 }
 
 variable "databricks_workspace_name" {
   type        = string
+  description = "Databricks Workspace Name"
   default     = "dbw-12taste-prod"
 }
